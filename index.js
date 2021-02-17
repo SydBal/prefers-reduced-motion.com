14458d301a1fc147d6570b87cfd5b35de7f9702a
SydBal <dominic.balassone@gmail.com>
2021-02-17 01:35:51 -0800

    css and content updates

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // START Darkmode
 window.toggleDarkmode = function () {
   var darkmodeButton = document.getElementById('darkmode-toggle')
+  var html = document.querySelector('html')
   if (!window.darkmode) {
     window.localStorage && window.localStorage.setItem('darkmode', true)
-    document.body.classList.add('darkmode')
+    html.classList.add('darkmode')
     darkmodeButton.setAttribute("aria-pressed", "true")
     window.darkmode = true
   } else {
     window.localStorage && window.localStorage.removeItem('darkmode')
-    document.body.classList.remove('darkmode')
+    html.classList.remove('darkmode')
     darkmodeButton.setAttribute("aria-pressed", "false")
     window.darkmode = false
   }
 }
 
 if (window.localStorage && window.localStorage.getItem('darkmode')) {
-  document.body.classList.add('darkmode')
+  document.querySelector('html').classList.add('darkmode')
   document.getElementById('darkmode-toggle').setAttribute("aria-pressed", "true")
   window.darkmode = true
 }
